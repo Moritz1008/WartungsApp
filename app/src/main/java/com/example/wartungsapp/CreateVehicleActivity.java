0ac7348b3524863e322b03bf855f794e7501f100
Riedel <mr-213796@rwu.de>
2023-06-05 20:15:47 +0200

    Edittext for Mileage now displays thousands separators for better readability (10000 -> 10.000)

--- app/src/main/java/com/example/wartungsapp/CreateVehicleActivity.java
+++ app/src/main/java/com/example/wartungsapp/CreateVehicleActivity.java
@@ -3,9 +3,12 @@ package com.example.wartungsapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.activity.result.ActivityResultCallback;
@@ -14,9 +17,14 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class CreateVehicleActivity extends AppCompatActivity {
 
     ActivityResultLauncher<String> takeImage;
+    public EditText editTextMileage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +57,15 @@ public class CreateVehicleActivity extends AppCompatActivity {
                 }
         );
 
-        //setup image button
-        //Button selectImage = findViewById(R.id.buttonSelectImage);
         ImageViewVehicle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 takeImage.launch("image/*");
             }
         });
+
+        editTextMileage = findViewById(R.id.editTextMileage);
+        editTextMileage.addTextChangedListener(onTextChangedListener());
     }
 
     @Override
@@ -67,5 +76,46 @@ public class CreateVehicleActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private TextWatcher onTextChangedListener() {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                editTextMileage.removeTextChangedListener(this);
+
+                try {
+                    String originalString = s.toString();
+
+                    Long longval;
+                    if (originalString.contains(".")) {
+                        originalString = originalString.replaceAll("\\.", "");
+                    }
+                    longval = Long.parseLong(originalString);
+
+                    DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+                    formatter.applyPattern("#,###,###,###");
+                    String formattedString = formatter.format(longval);
+                    formattedString = formattedString.replaceAll(",", ".");
+
+
+                    //setting text after format to EditText
+                    editTextMileage.setText(formattedString);
+                    editTextMileage.setSelection(editTextMileage.getText().length());
+                } catch (NumberFormatException nfe) {
+                    nfe.printStackTrace();
+                }
+
+                editTextMileage.addTextChangedListener(this);
+            }
+        };
     }
 }
